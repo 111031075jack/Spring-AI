6cab2e1b7c2dee334ddac0002ac6969a6ec76234
111031075jack <111031075@live.asia.edu.tw>
2025-11-29 12:19:42 +0800

    update

---
+++ src/main/java/com/example/demo/service/OllamaService.java
@@ -0,0 +1,44 @@
+package com.example.demo.service;
+
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OllamaService {
+
+	private final OllamaChatModel chatModel;
+	
+	// Spring 自動注入機制會利用建構子注入參數
+	public OllamaService(OllamaChatModel chatModel) {
+		this.chatModel = chatModel;
+	}
+	
+	// 使用預設模型的 ask
+	public String ask(String q) {
+		return chatModel.call(q);
+	}
+	
+	// 可以指定模型的 ask
+	public String ask(String q, String useModel) {
+		if(useModel == null) {
+			return ask(q);
+		}
+		 /* 電腦沒下載這個模型, 所以註解掉
+		if(useModel.contains("finance")) {
+			useModel = "martain7r/finance-llama-8b:fp16";
+		}
+		*/
+		// 變更模型
+		OllamaChatOptions options = OllamaChatOptions.builder()
+				.model(useModel)
+				.build();
+		// 咒語
+		Prompt prompt = new Prompt(q, options);
+		
+		return chatModel.call(prompt).getResult().getOutput().getText();
+		
+	}
+	
+}
---
+++ src/main/resources/static/js/chat.js
@@ -0,0 +1,63 @@
+const chatBox = document.getElementById("chat-box");
+const questionInput = document.getElementById("question");
+
+// 一次性回傳: 呼叫 /ollama/ask
+function ask(){
+	const question = questionInput.value.trim();
+		
+	if(!question) return;
+	
+	chatBox.innerText += `You: ${question}\n`;
+	questionInput.value = '';
+	
+	fetch(`http://localhost:8080/ollama/ask?q=${encodeURIComponent(question)}`)
+		.then(res => res.text())
+		.then(answer => {
+			chatBox.innerText += `AI: ${answer}\n\n`;
+			chatBox.scrollTo(0, chatBox.scrollHeight);
+		})
+		.catch(err => {
+			chatBox.innerText += "錯誤呼叫\n";
+			console.error(err);
+		});
+		
+}
+
+// 逐字回答: 呼叫 /ollama/stream
+// 透過 EventSource 可以不間斷得持續收到 Server 端傳來得訊息直到 close 為止
+// 每收到一個訊息就會觸發 onmessage
+function stream(){
+	const question = questionInput.value.trim();
+			
+	if(!question) return;
+	
+	chatBox.innerText += `You: ${question}\n`;
+	questionInput.value = '';
+		
+	// 建立 SSE 連線(Server-Send Events)
+	// - /ollama/stream => 後端串流 API
+	const eventSource = new EventSource(`/ollama/stream?q=${encodeURIComponent(question)}`);
+	
+	// 先在 chatBox 上印出 AI: 當作前贅字
+	chatBox.innerText += 'AI: ';
+	
+	// 當後端送出一個事件資料(chunk) 時, 就會觸發 onmessage
+	eventSource.onmessage = function(event){
+		chatBox.innerText += event.data;
+		// 讓卷軸能自動滾到最底下, 確保使用者能看到最新內容
+		chatBox.scrollTo(0, chatBox.scrollHeight);
+	};
+	
+	// 當連線失敗或後端關閉時就會觸發 onerror
+	eventSource.onerror = function(err){
+		// 關閉 eventSource 連線 
+		eventSource.close();
+		
+		chatBox.innerText += "\n[Stream end]\n\n";
+		chatBox.scrollTo(0, chatBox.scrollHeight);
+	};
+	
+	
+}
+
+		
